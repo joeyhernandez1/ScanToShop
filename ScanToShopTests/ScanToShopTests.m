fc2135d6639084d89fec7150faa0a3d9e5bf8e96
Joey R. Hernandez Perez <joeyrhernandez1@fb.com>
2020-07-13 17:31:07 -0400

    Initial Commit

---
+++ ScanToShopTests/ScanToShopTests.m
@@ -0,0 +1,37 @@
+//
+//  ScanToShopTests.m
+//  ScanToShopTests
+//
+//  Created by Joey R. Hernandez Perez on 7/13/20.
+//  Copyright © 2020 joeyrhernandez1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScanToShopTests : XCTestCase
+
+@end
+
+@implementation ScanToShopTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
